cb40e6c3c63d9cff8b375aeec3b33fd3fa9588c1
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-25 21:36:57 +0500

    WIp

---
+++ lib/addverts_page.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class AddvertsPage extends StatelessWidget {
+  const AddvertsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: ListView(),
+      ),
+    );
+  }
+}
