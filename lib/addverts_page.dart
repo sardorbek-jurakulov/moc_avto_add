687d19f3b94c7b253485b61b46f1b9d32afc4f42
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-26 09:00:01 +0500

    WIP

--- lib/addverts_page.dart
+++ lib/addverts_page.dart
@@ -7,7 +7,42 @@ class AddvertsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+            title: Row(children: [
+          Stack(
+            children: [
+              Icon(Icons.menu),
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+                child: const Text(
+                  "1",
+                  style: TextStyle(
+                    fontSize: 8,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text("Avtoelon"),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ])),
         body: ListView(),
       ),
     );
