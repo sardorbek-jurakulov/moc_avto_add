445c29816d127935c316919460df2a60eb5d51ea
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-26 18:07:05 +0500

    WIP

--- lib/addverts_page.dart
+++ lib/addverts_page.dart
@@ -22,53 +22,56 @@ class AddvertsPage extends StatelessWidget {
             Container(
               width: MediaQuery.of(context).size.width * 0.35,
               height: double.infinity,
-              child: Image.asset(addImagePath, fit: BoxFit.contain),
+              child: Image.asset(addImagePath, fit: BoxFit.cover),
             ),
             Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    addTitle,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF444444),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      addTitle,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF444444),
+                      ),
                     ),
-                  ),
-                  Text(
-                    addDescription,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Color(0xFF444444),
+                    Text(
+                      addDescription,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Color(0xFF444444),
+                      ),
                     ),
-                  ),
-                  Text(
-                    addAddressAndPostedDate,
-                    style: TextStyle(
-                      color: Color(0xFFAAAAAA),
-                      fontSize: 13,
+                    Text(
+                      addAddressAndPostedDate,
+                      style: const TextStyle(
+                        color: Color(0xFFAAAAAA),
+                        fontSize: 13,
+                      ),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      if (imagesCount > 0) ...[
-                        const Icon(
-                          Icons.photo_camera,
-                          color: Color(0xFFAAAAAA),
-                          size: 20,
-                        ),
-                        Text(imagesCount.toString(),
-                            style: TextStyle(
-                              color: Color(0xFF999999),
-                              fontSize: 12,
-                            )),
-                      ] else
-                        ...[],
-                    ],
-                  ),
-                ],
+                    Row(
+                      children: [
+                        if (imagesCount > 0) ...[
+                          const Icon(
+                            Icons.photo_camera,
+                            color: Color(0xFFAAAAAA),
+                            size: 20,
+                          ),
+                          Text(imagesCount.toString(),
+                              style: const TextStyle(
+                                color: Color(0xFF999999),
+                                fontSize: 12,
+                              )),
+                        ] else
+                          ...[],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -174,15 +177,81 @@ class AddvertsPage extends StatelessWidget {
                 "Olot, 2 Oktabr",
                 1),
             const Divider(
-              height: 0,
-              thickness: 2,
-              color: Color(0xFFEEEEEE),
+              height: 10,
+              thickness: 1,
+              color: Color(0xFFDDDDDD),
             ),
             createCarCard(
                 "assets/images/spark.jpeg",
                 "Chevrolet Spark 2020",
                 "2020\"yil.,\"Hatchback, 1.25\"l., \"avtomat, benzin/gaz",
-                "Termiz, 29 fevral",
+                "Termiz, 1 Oktyabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/onix.jpeg",
+                "Chevrolet Onix 2022",
+                "2022\"yil.,\"sedan, 1.8\"l., \"avtomat, benzin",
+                "Toshkent, 29 Sentabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/caddy.jpeg",
+                "Volkswagen Caddy 2021",
+                "2021\"yil.,\"Suv, 1.6\"l., \"mexanika, benzin/gaz",
+                "Guliston, 29 Sentabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/accent.jpeg",
+                "Hunday Accent 2022",
+                "2022\"yil.,\"Sedan, 1.8\"l., \"avtomat, benzin",
+                "Samarqand, 27 Sentabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/captiva.jpeg",
+                "Chevrolet Captiva 2018",
+                "2018\"yil.,\"Cuv, 2.4\"l., \"avtomat, benzin/gaz",
+                "Qarshi, 15 Sentabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/santafe.jpeg",
+                "Hunday Santafe 2022",
+                "2022\"yil.,\"Cuv, 3.2\"l., \"avtomat, benzin",
+                "Qibray, 12 Sentabr",
+                0),
+            const Divider(
+              height: 0,
+              thickness: 2,
+              color: Color(0xFFEEEEEE),
+            ),
+            createCarCard(
+                "assets/images/tucson.jpeg",
+                "Hunday Tucson 2022",
+                "2022\"yil.,\"Cuv, 3.2\"l., \"avtomat, benzin",
+                "Toshkent, 11 Sentabr",
                 0),
             const Divider(
               height: 0,
