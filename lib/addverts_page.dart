3def84041481dfb81fa7880f8d5497c0e5123267
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-26 11:50:56 +0500

    WIP

--- lib/addverts_page.dart
+++ lib/addverts_page.dart
@@ -8,49 +8,85 @@ class AddvertsPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-            title: Row(children: [
-          Stack(
-            clipBehavior: Clip.none,
-            // fit: StackFit.expand,
-            // clipBehavior: Clip.antiAliasWithSaveLayer,
-            // overflow: Overflow.visible,
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Icon(Icons.menu),
-              Positioned(
-                bottom: 8,
-                left: 10,
-                child: Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: const BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
-                  ),
-                  child: const Text(
-                    "1",
-                    style: TextStyle(
-                      fontSize: 10,
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  const Icon(Icons.menu),
+                  Positioned(
+                    bottom: 8,
+                    left: 10,
+                    child: Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Text(
+                        "1",
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
                     ),
                   ),
+                ],
+              ),
+              const Text(
+                "Avtoelon",
+                style: TextStyle(
+                  fontSize: 24,
+                  letterSpacing: 1.5,
                 ),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: const CircleBorder(),
+                  // fixedSize: const Size(5, 5),
+                  side: const BorderSide(
+                    color: Colors.white,
+                    width: 0.5,
+                  ),
+                  minimumSize: const Size(10, 10), // Set this
+                  padding: EdgeInsets.zero, // and this
+                ),
+                onPressed: () {},
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: const CircleBorder(),
+                  // fixedSize: const Size(5, 5),
+                  side: const BorderSide(
+                    color: Colors.white,
+                    width: 0.5,
+                  ),
+                  minimumSize: const Size(15, 15), // Set this
+                  padding: EdgeInsets.zero, // and this
+                ),
+                onPressed: () {},
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 25,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
               ),
             ],
           ),
-          Text("Avtoelon"),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.add,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-          ),
-        ])),
+        ),
         body: ListView(),
       ),
     );
